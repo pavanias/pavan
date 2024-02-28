bfe67593a3e348a4e058ff92e4fa27e70051f2fc
pavanias <159236247+pavanias@users.noreply.github.com>
2024-02-28 12:32:16 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,6 +5,6 @@ public class hello.java
 		System.out.println("wellcome to java,bash");
 		System.out.println("wellcome to mosurri ");
                 System.out.println("wellcome to kasakasthan ");
-		System.out.println("wellcome to goa ");
+		System.out.println("wellcomekjk to goa ");
 	}
 }
